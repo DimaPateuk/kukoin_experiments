921aede1b4d4dc54590c69425941694fc3c282e7
dpatseyuk <dpatseyuk@exadel.com>
2022-10-13 20:02:25 +0300

    reafactoring

--- strategy.js
+++ strategy.js
@@ -21,8 +21,8 @@ const { Strategy } =require('./strategyNew');
 
 
 let count = 0;
-const maxStrategyTries = 5;
-const maxStrategiesInParallel = 5;
+const maxStrategyTries = 1;
+const maxStrategiesInParallel = 1;
 const strategiesInProgress = new Map();
 const executedStrategies = [];
 
