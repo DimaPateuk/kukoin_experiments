a22e6fb59c2f8751fb49897fe1412e6e6de40f31
dpatseyuk <dpatseyuk@exadel.com>
2022-10-18 15:26:18 +0300

    reafactoring

--- strategy.js
+++ strategy.js
@@ -70,7 +70,7 @@ function doRealStrategy(currentStrategy, orderBookDepth) {
     return;
   }
 
-  if (profitInfo.profit > 0.005) {
+  if (profitInfo.profit > 0 && profitInfo.profit < 0.0003) {
     startStrategy(currentStrategy, profitInfo);
   }
 }
