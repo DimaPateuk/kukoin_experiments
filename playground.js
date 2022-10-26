b362cbc093f65e9a49222ea7ecc68420228905f7
dpatseyuk <dpatseyuk@exadel.com>
2022-10-26 11:00:41 +0300

    reafactoring

--- playground.js
+++ playground.js
@@ -1,18 +1,4 @@
-var term = require( 'terminal-kit' ).terminal ;
+const { v4 } = require('uuid');
 
-var fs = require( 'fs' ) ;
 
-term.cyan( 'Choose a file:\n' ) ;
-
-var items = ['sell', 'buy'] ;
-
-term.gridMenu( items , function( error , response ) {
-  console.log(response);
-
-	process.exit() ;
-} ) ;
-
-setInterval(() => {
-  console.clear();
-  console.log(1);
-}, 10)
+console.log(v4());
