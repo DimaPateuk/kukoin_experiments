d8706c5989a77c5cd0ae7fb5351a285a9b80466d
dpatseyuk <dpatseyuk@exadel.com>
2022-10-27 23:56:59 +0300

    reafactoring

--- resources.js
+++ resources.js
@@ -31,7 +31,6 @@ const strategies = Object
     return allowed.includes(value[0].split('-')[0]);
   })
   .map(entry => entry[1]);
-console.log(strategies);
 
 const allSymbols = [
   // 'TRX-USDT', 'WIN-TRX', 'WIN-USDT'
