ae60b2ca3d087f6266696f05dbdef49361eedaaf
dpatseyuk <dpatseyuk@exadel.com>
2022-11-02 10:45:29 +0300

    reafactoring

--- placeOrder.js
+++ placeOrder.js
@@ -2,7 +2,7 @@ const kucoin = require('./kucoin');
 const { Subject } = require('rxjs');
 
 const placeOrderErrorSubject = new Subject();
-function placeOrder(params, count) {
+function placeOrder(params, count = 0) {
   let promise;
   if (params.stopPrice) {
     promise = kucoin.placeStopOrder({
@@ -21,26 +21,30 @@ function placeOrder(params, count) {
   }
 
   return promise.then((res) => {
-      if (res.code !== '200000') {
-        placeOrderErrorSubject.next({
-          params,
-          res
-        });
-
-        console.log('tries', count, res, params);
-        process.exit(1);
-      }
-      return res;
-
-    }, (err) => {
-      console.log(err, params);
+    if (res.code === '200004' && count < 3) {
+      console.log('retry of', params, count, 'because of', res);
+      return placeOrder(params, count + 1);
+    }
+    if (res.code !== '200000') {
       placeOrderErrorSubject.next({
         params,
-        err,
+        res
       });
 
+      console.log('tries', count, res, params);
       process.exit(1);
+    }
+    return res;
+
+  }, (err) => {
+    console.log(err, params);
+    placeOrderErrorSubject.next({
+      params,
+      err,
     });
+
+    process.exit(1);
+  });
 }
 
 module.exports = { placeOrder, placeOrderErrorSubject }
