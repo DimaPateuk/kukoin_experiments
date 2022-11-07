c1840c0ebe2d1679455071ccef9d84feb173a4e8
dpatseyuk <dpatseyuk@exadel.com>
2022-11-08 00:35:43 +0300

    reafactoring

--- calcProfit.js
+++ calcProfit.js
@@ -47,17 +47,11 @@ function parseSizes(currentStrategy, depth) {
 function getStringPrices(currentStrategy, depth) {
   const [buy, buy2, sell] = currentStrategy;
 
-  console.log(
-    buy,
-    processPrice((parseFloat(symbolsOrderBookInfoMap[buy].asks[depth][0]) * 0.99).toString(), buy),
-    (parseFloat(symbolsOrderBookInfoMap[buy].asks[depth][0]) * 0.99).toString(),
-    symbolsOrderBookInfoMap[buy].asks[depth][0]
-  )
 
   return [
     processPrice((parseFloat(symbolsOrderBookInfoMap[buy].asks[depth][0]) * 0.99).toString(), buy),
-    symbolsOrderBookInfoMap[buy2].asks[depth][0],
-    symbolsOrderBookInfoMap[sell].bids[depth][0]
+    symbolsOrderBookInfoMap[buy2].bids[depth + 45][0],
+    symbolsOrderBookInfoMap[sell].asks[depth + 45][0]
   ];
 }
 const baseFirstStepAmount = 2;
