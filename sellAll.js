56770d5c43d6e6780446a45516f132a294a2c67d
dpatseyuk <dpatseyuk@exadel.com>
2022-11-07 23:54:15 +0300

    reafactoring

--- sellAll.js
+++ sellAll.js
@@ -5,69 +5,65 @@ const { symbolsInfo } = require('./resources');
 const { v4 } = require('uuid');
 
 
-function sellAll(onEnd) {
+async function sellAll(onEnd) {
   console.log('------------------------------- sellAll');
   const balanceInfo = [];
-  kucoin.cancelAllOrders({});
-  setTimeout(() => {
-    kucoin.getAccounts()
-      .then(response => {
+  await kucoin.cancelAllOrders({});
+  await kucoin.getAccounts()
+    .then(response => {
 
-        response.data.forEach(item => {
-          if (item.type !== 'trade') {
-            return;
-          }
-
-          if (item.available === '0') {
-            return;
-          }
-
-          if (['KCS', 'USDT'].includes(item.currency)) {
-            return;
-          }
-          const symbol = `${item.currency}-USDT`;
-          const symbolI = symbolsInfo[symbol];
-          if (!symbolI) {
-            return;
-          }
-
-          if (parseFloat(symbolI.baseMinSize) > parseFloat(item.available)) {
-            return;
-          }
-
-          balanceInfo.push(item);
-        });
-
-      });
-  }, 5000);
-
-  setTimeout(() => {
-    balanceInfo.forEach((item, index) => {
-      setTimeout(() => {
-        const symbol = `${item.currency}-USDT`;
-        const sellAmount = processNumber(item.available, symbol, 'bids');
-
-        kucoin.placeOrder({
-          type: 'market',
-          clientOid: v4(),
-          side: 'sell',
-          symbol: symbol,
-          size: sellAmount,
-        })
-        .then((e) => {
-          console.log(e);
-          console.log(symbol, sellAmount);
-
-        }, console.log);
-
-        if (index === balanceInfo.length - 1) {
-          setTimeout(() => {
-            onEnd();
-          }, 1500)
+      response.data.forEach(item => {
+        if (item.type !== 'trade') {
+          return;
         }
-      }, index * 200);
+
+        if (item.available === '0') {
+          return;
+        }
+
+        if (['KCS', 'USDT'].includes(item.currency)) {
+          return;
+        }
+        const symbol = `${item.currency}-USDT`;
+        const symbolI = symbolsInfo[symbol];
+        if (!symbolI) {
+          return;
+        }
+
+        if (parseFloat(symbolI.baseMinSize) > parseFloat(item.available)) {
+          return;
+        }
+
+        balanceInfo.push(item);
+      });
+
+
+      balanceInfo.forEach((item, index) => {
+        setTimeout(() => {
+          const symbol = `${item.currency}-USDT`;
+          const sellAmount = processNumber(item.available, symbol, 'bids');
+
+          kucoin.placeOrder({
+            type: 'market',
+            clientOid: v4(),
+            side: 'sell',
+            symbol: symbol,
+            size: sellAmount,
+          })
+            .then((e) => {
+              console.log(e);
+              console.log(symbol, sellAmount);
+
+            }, console.log);
+
+          if (index === balanceInfo.length - 1) {
+            setTimeout(() => {
+              onEnd();
+            }, 1500)
+          }
+        }, index * 200);
+      });
     });
-  }, 10000);
 }
 
 module.exports = {
